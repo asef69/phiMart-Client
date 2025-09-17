898d577291f40e95e92a8f3d9092ed76b72d7516
Kazi Asef Kabir <asefkabir97@gmail.com>
2025-09-17 16:49:25 +0600

    module-34

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,14 @@
 import { Route, Routes } from "react-router";
-import About from "../Pages/About";
+import About from "../pages/About";
 import MainLayout from "../layouts/MainLayout";
-import Home from "../Pages/Home";
-import Shop from "../Pages/Shop";
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import PrivateRoute from "../components/PrivateRoute";
+import ActivateAccount from "../components/Registration/ActivateAccount";
+
 const AppRoutes = () => {
   return (
     <Routes>
@@ -13,6 +19,17 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="activate/:uid/:token" element={<ActivateAccount />} />
+        <Route
+          path="dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Route>
     </Routes>
   );
