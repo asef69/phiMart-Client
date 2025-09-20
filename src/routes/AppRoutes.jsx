bae78f00580f996c85dc28de653fc760613d82a1
Kazi Asef Kabir <asefkabir97@gmail.com>
2025-09-20 18:50:22 +0600

    module-35

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,17 +3,18 @@ import About from "../pages/About";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
-import Login from "../Pages/Login";
-import Register from "../Pages/Register";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 import Dashboard from "../Pages/Dashboard";
 import PrivateRoute from "../components/PrivateRoute";
 import ActivateAccount from "../components/Registration/ActivateAccount";
+import DashboardLayout from "../layouts/DashboardLayout";
+import Profile from "../Pages/Profile";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      {/* <Route index element={<Home />}></Route>
-      <Route path="about" element={<About />} /> */}
+      {/* Public Routes  */}
 
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
@@ -22,14 +23,18 @@ const AppRoutes = () => {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="activate/:uid/:token" element={<ActivateAccount />} />
-        <Route
-          path="dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
+      </Route>
+      {/* Private Routes  */}
+      <Route
+        path="dashboard"
+        element={
+          <PrivateRoute>
+            <DashboardLayout />
+          </PrivateRoute>
+        }
+      >
+        <Route index element={<Dashboard />} />
+        <Route path="profile" element={<Profile />} />
       </Route>
     </Routes>
   );
